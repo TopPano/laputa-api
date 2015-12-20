affd77440564e76dcefd0671f76c54c1686a42db
wettingchou <wtchou1209@gmail.com>
2015-12-20 21:35:27 +0800

    update configuration files

--- server/config.local.js
+++ server/config.local.js
@@ -1,30 +1,15 @@
+/*
+ * This file exists for the fact that we can't have 'config.js' to perform
+ * programmatic ations for default configuration. This file will always be
+ * loaded if exists, either JSON or JS. And it will override the settings of
+ * 'config.json'. For more information, please read:
+ * https://github.com/strongloop/loopback-boot/issues/101
+ */
+ 'use strict';
+
 var p = require('../package.json');
 var version = p.version.split('.').shift();
 
 module.exports = {
   restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
-  host: '0.0.0.0',
-  port: 3000,
-  remoting: {
-    context: {
-      enableHttpContext: false
-    },
-    rest: {
-      normalizeHttpPath: false,
-      xml: false
-    },
-    json: {
-      strict: false,
-      limit: '100kb'
-    },
-    urlencoded: {
-      extended: true,
-      limit: '100kb'
-    },
-    cors: false,
-    errorHandler: {
-      disableStackTrace: false
-    }
-  },
-  legacyExplorer: false
 };
