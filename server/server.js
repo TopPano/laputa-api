4a82bc90994a78a88bcd6326c36f9e6562f4889f
wettingchou <wtchou1209@gmail.com>
2015-12-23 16:46:48 +0800

    use current user id as a literal

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ app.start = function() {
   });
 };
 
+app.use(loopback.token({currentUserLiteral: 'me'}));
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
