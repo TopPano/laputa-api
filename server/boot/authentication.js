99721fefff52ba68ffee264233c9d3cbff49c6f9
wettingchou <wtchou1209@gmail.com>
2016-04-28 10:54:40 +0800

    extend token ttl

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,4 +1,23 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+
+  // check for access token in cookies, headers, and query string parameters
+  server.use(loopback.token({currentUserLiteral: 'me'}));
+
+  // keep alive the token if it is active
+  server.use(function(req, res, next) {
+    var token = req.accessToken;
+    if (!token) { return next(); }
+    var now = new Date();
+    var time = now.getTime() - token.created.getTime();
+    // don't extend the token if it has expired or just been issued less than a week
+    // NOTE: ttl should be longer than a week
+    if ( time > req.accessToken.ttl || time  < 604800 ) {
+      return next();
+    }
+    req.accessToken.created = now;
+    req.accessToken.ttl = 2419200; // four week
+    req.accessToken.save(next);
+  });
 };
--- server/server.js
+++ server/server.js
@@ -23,7 +23,6 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 });
 
-app.use(loopback.token({currentUserLiteral: 'me'}));
 app.use(multer({storage: multer.memoryStorage()}).fields([
   { name: 'thumbnail', maxCount: 1 },
   { name: 'image', maxCount: 1 }
