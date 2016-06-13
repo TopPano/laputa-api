473ba2926efb00003e5579d2e3a801d64097d03f
wettingchou <wtchou1209@gmail.com>
2016-06-13 23:51:21 +0800

    update datasource.dev.js

--- server/datasources.dev.js
+++ server/datasources.dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   db: {
     connector: 'mongodb',
-    url: process.env.DB_URL || 'mongodb://localhost:27017/verpix-dev-db?allowExtendedOperators=true',
+    url: process.env.DB_URL || 'mongodb://localhost:27017/verpix-dev-db',
     allowExtendedOperators: true
   }
 };
