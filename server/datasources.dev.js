ab80b740f9e42ee7cb019d30dfd837716bd122bf
wettingchou <wtchou1209@gmail.com>
2016-06-13 19:10:35 +0800

    update datasources.dev.js

--- server/datasources.dev.js
+++ server/datasources.dev.js
@@ -1,11 +1,7 @@
 module.exports = {
   db: {
     connector: 'mongodb',
-    hostname: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 27017,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'verpix-dev-db',
+    url: process.env.DB_URL || 'mongodb://localhost:27017/verpix-dev-db?allowExtendedOperators=true',
     allowExtendedOperators: true
   }
 };
